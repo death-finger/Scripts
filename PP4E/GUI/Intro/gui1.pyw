c702dc7acdb90b7f527b58081adc2cdf96426e8a
Joshua Pu <death_finger@sina.com>
2016-03-02 23:34:11 +0800

    20160302 23:34

---
+++ PP4E/GUI/Intro/gui1.pyw
@@ -0,0 +1,6 @@
+# e.g. 7-2
+
+from tkinter import Label
+widget = Label(None, text='Hello GUI world!')
+widget.pack()
+widget.mainloop()
---
+++ PP4E/GUI/Intro/gui3b.py
@@ -0,0 +1,11 @@
+# e.g. 7-13
+
+import sys
+from tkinter import *
+
+
+widget = Button(None,
+                text = 'Hello event world',
+                command=(lambda: print('Hello lambda world') or sys.exit()))
+widget.pack()
+widget.mainloop()
