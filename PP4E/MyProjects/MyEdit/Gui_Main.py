900538bb2d18bd2515cca98c140bd30cc67ca192
Joshua Pu <death_finger@sina.com>
2016-03-16 17:20:09 +0800

    20160316 17:20

--- PP4E/MyProjects/MyEdit/Gui_Main.py
+++ PP4E/MyProjects/MyEdit/Gui_Main.py
@@ -6,48 +6,107 @@
 ########################################################################
 """
 
+# 导入通用组件
+
 from tkinter import *
-from tkinter.ttk import *
+import os, sys
 
 
-class MyMenu(Menu):
-    def __init__(self, parent=None):
-        top = Menu(parent)
-        parent.config(menu=top)
-        self.makeMenu(top)
+# 主体框架
 
-    def makeMenu(self, parent):
-        # File Menu
-        file = Menu(parent, tearoff=True)
-        file.add_command(label='Open', command=self.notdone, underline=0)
-        file.add_command(label='Save', command=self.notdone, underline=0)
-        file.add_command(label='Save As', command=self.notdone, underline=5)
-        file.add_command(label='New', command=self.notdone, underline=0)
-        file.add_separator()
-        file.add_command(label='Quit', command=self.notdone, underline=0)
-        parent.add_cascade(label='File', menu=file, underline=0)
+# 导入文本框组件
+from tkinter.scrolledtext import ScrolledText
 
-        # Edit Menu
-        edit = Menu(parent, tearoff=True)
-        edit.add_command(label='Undo', command=self.notdone, underline=0)
-        edit.add_command(label='Redo', command=self.notdone, underline=0)
-        edit.add_separator()
-        edit.add_command(label='Cut', command=self.notdone, underline=0)
-        edit.add_command(label='Copy', command=self.notdone, underline=0)
-        edit.add_command(label='Paste', command=self.notdone, underline=0)
-        edit.add_separator()
-        edit.add_command(label='Delete', command=self.notdone, underline=0)
-        edit.add_command(label='Select All', command=self.notdone, underline=0)
-        parent.add_cascade(label='Edit', menu=edit, underline=0)
+class TextEditorMainFrame:
+    def __init__(self):
+        # 初始化设定
+        self.version_set()
+        self.file_path = '/Users/joshuapu/Documents/Scripts/PP4E/MyProjects/MyEdit'
+        # 主窗口
+        self.main = Tk()
+        self.main.title('MyEdit %.1f' % self.version)
+        # 主菜单
+        self.menu = Menu(self.main)
+        self.main.config(menu=self.menu)
+        makeMenu(self.menu)
+        # 主体部分, 包含文件路径栏, Text框以及Scrollbar
+        self.makeFrame(self.main)
+
+    def makeFrame(self, parent):
+        frame_main = Frame(parent)
+        frame_main.pack(expand=YES, fill=BOTH)
+        # 文件路径显示
+        label_path = Label(frame_main, text=self.file_path)
+        label_path.config(fg='white', bg='black', font=('times', 10, 'italic'))
+        label_path.pack(side=TOP, fill=X)
+        # 主Text窗口
+        text_main = Text(frame_main)
+        text_main_yscroll = Scrollbar(frame_main, command=text_main.yview, relief=SUNKEN)
+        text_main_yscroll.pack(side=RIGHT, fill=X)
+        text_main.config(yscrollcommand=text_main_yscroll.set)
+        text_main_xscroll = Scrollbar(frame_main, command=text_main.xview, orient=HORIZONTAL)
+        text_main_xscroll.pack(side=BOTTOM, fill=X)
+        text_main.config(xscrollcommand=text_main_xscroll.set)
+        text_main.pack(side=TOP, expand=YES, fill=BOTH)
+
+    ############################
+    # 菜单按钮功能设定
+    ############################
+    def onFileOpen(self):
+        self.file_path = askopenfilename()
 
 
+    ############################
+    # 额外信息显示
+    ############################
+    def version_set(self):
+        self.version = 1.00
 
-    def notdone(self):
-        pass
+# 导入菜单制作工具
+
+from makeGuiTool import MakeGui
+from tkinter.filedialog import askopenfilename, asksaveasfilename
+from tkinter.messagebox import showinfo
+
+
+class makeMenu(MakeGui):
+    def start(self):
+        self.menuBar = [('File', 0, [('Open...', 0, askopenfilename),
+                                     ('Save', 0, lambda: None),
+                                     ('Save As...', 5, asksaveasfilename),
+                                     ('New', 0, lambda: None),
+                                     'separator',
+                                     ('Quit', 0, sys.exit)]),
+                        ('Edit', 0, [('Undo', 0, lambda: None),
+                                     ('Redo', 0, lambda: None),
+                                     'separator',
+                                     ('Cut', 0, lambda: None),
+                                     ('Copy', 0, lambda: None),
+                                     ('Paste', 0, lambda: None),
+                                     'separator',
+                                     ('Delete', 0, lambda: None),
+                                     ('Select All', 7, lambda: None)]),
+                        ('Search', 0, [('Goto...', 0, lambda: None),
+                                       ('Find...', 0, lambda: None),
+                                       ('Refind', 0, lambda: None),
+                                       ('Change...', 0, lambda: None),
+                                       ('Grep...', 0, lambda: None)]),
+                        ('Tools', 0, [('Pick Font...', 0, lambda: None),
+                                      ('Font List', 0, lambda: None),
+                                      'separator',
+                                      ('Pick Bg...', 5, lambda: None),
+                                      ('Pick Fg...', 5, lambda: None),
+                                      ('Color List', 0, lambda: None),
+                                      'separator',
+                                      ('Info...', 0, lambda: None),
+                                      ('Clone', 0, lambda: None),
+                                      ('Run Code', 0, lambda: None)])]
+
+    def help(self):
+        showinfo("Help Info", 'No help for now...')
 
 
 
 if __name__ == '__main__':
-    root = Tk()
-    MyMenu(root)
-    root.mainloop()
+    TextEditorMainFrame()
+    mainloop()
--- PP4E/MyProjects/MyEdit/makeGuiTool.py
+++ PP4E/MyProjects/MyEdit/makeGuiTool.py
@@ -1,44 +1,94 @@
 from tkinter import *
-from tkinter.ttk import *
 
-class makeGui():
+
+class MakeGui:
     menuBar = []
+    helpButton = True
+
     def __init__(self, parent=None):
+        self.start()
         self.makeMenuBar(parent, self.menuBar)
 
-    def makeMenuBar(self, parent, menulist):
-        for (name, key, items) in menulist:
-            name = Menu(parent)
-            self.makeMenuItems(name, items)
-            parent.add_cascade(label=name, underline=key, menu=name)
+    def makeMenuBar(self, parent, menuBar):
+        for (name, key, items) in menuBar:
+            menu_main = Menu(parent)
+            self.makeMenuItems(menu_main, items)
+            parent.add_cascade(label=name, underline=key, menu=menu_main)
 
-    def makeMenuItems(self, name, items):
+        if self.helpButton:
+            parent.add_command(label='Help', underline=0, command=self.help)
+
+
+    def makeMenuItems(self, menu_name, items):
         for item in items:
             if item == 'separator':
-                name.add_separator()
+                menu_name.add_separator()
             elif type(item) == list:
                 for num in item:
-                    name.entryconfig(num, status=DISABLED)
-            elif  type(item[2]) != list:
-                name.add_command(label=item[0],
-                                 underline=item[1],
-                                 command=item[2])
+                    #menu_name.entryconfig(num, state=DISABLED)
+                    pass
+            elif type(item[2]) != list:
+                menu_name.add_command(label=item[0],
+                                      underline=item[1],
+                                      command=item[2])
             else:
-                sub_name = Menu(name)
+                sub_name = Menu(menu_name)
                 self.makeMenuBar(sub_name, item)
+
+    def help(self):
+        raise NotImplementedError
 
     def start(self):
         raise NotImplementedError
 
 
 if __name__ == '__main__':
-    menuBar = [('File', 0, [('Open', 0, lambda: 0), ('Quit', 0, sys.exit)]),
-               ('Edit', 0, [('Cut', 0, lambda: 0), ('Paste', 0, lambda: 0)])]
+    from tkinter.filedialog import askopenfilename, asksaveasfilename
+    from tkinter.messagebox import showinfo
 
-    class Test(makeGui):
+    menuBar = [('File', 0, [('Open...', 0, askopenfilename),
+                                     ('Save', 0, lambda: None),
+                                     ('Save As...', 5, asksaveasfilename),
+                                     ('New', 0, lambda: None),
+                                     'separator',
+                                     ('Quit', 0, sys.exit),
+                                     [1, 3]]),
+                        ('Edit', 0, [('Undo', 0, lambda: None),
+                                     ('Redo', 0, lambda: None),
+                                     'separator',
+                                     ('Cut', 0, lambda: None),
+                                     ('Copy', 0, lambda: None),
+                                     ('Paste', 0, lambda: None),
+                                     'separator',
+                                     ('Delete', 0, lambda: None),
+                                     ('Select All', 7, lambda: None),
+                                     [0, 1, 2, 3, 4, 5, 6]]),
+                        ('Search', 0, [('Goto...', 0, lambda: None),
+                                       ('Find...', 0, lambda: None),
+                                       ('Refind', 0, lambda: None),
+                                       ('Change...', 0, lambda: None),
+                                       ('Grep...', 0, lambda: None),
+                                       [0, 1, 2, 3, 4]]),
+                        ('Tools', 0, [('Pick Font...', 0, lambda: None),
+                                      ('Font List', 0, lambda: None),
+                                      'separator',
+                                      ('Pick Bg...', 5, lambda: None),
+                                      ('Pick Fg...', 5, lambda: None),
+                                      ('Color List', 0, lambda: None),
+                                      'separator',
+                                      ('Info...', 0, lambda: None),
+                                      ('Clone', 0, lambda: None),
+                                      ('Run Code', 0, lambda: None),
+                                      [0, 1, 2, 3, 4, 5, 6, 7]])]
+
+    class Test(MakeGui):
         def start(self):
             self.menuBar = menuBar
+        def help(self):
+            showinfo('Help Info', 'No help offered!')
 
     root = Tk()
-    Test(root)
+    top = Menu(root)
+    root.config(menu=top)
+    Test(top)
     mainloop()
