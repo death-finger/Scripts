c469a95c45ac73736ed8ff62ec0e0074e1d3409c
Joshua Pu <death_finger@sina.com>
2016-04-07 23:11:31 +0800

    20160407 2310

---
+++ PP4E/Internet/Email/mailconfig.py
@@ -0,0 +1,21 @@
+# e.g. 13-17
+
+popservername = 'pop.sina.com'
+popusername = 'death_finger@sina.com'
+
+smtpservername = 'smtp.sina.com'
+
+myaddress = 'death_finger@sina.com'
+mysignature = ('Best Regrads,\nJoshua Pu')
+
+smtpuser = None
+smtppasswdfile = ''
+poppasswdfile = ''
+
+sentmailfile = r'./sentmail.txt'
+savemailfile = r'./savemail.txt'
+
+fetchEncoding = 'utf8'
+headersEncodeTo = None
+
+fetchlimit = 25
---
+++ PP4E/Internet/Email/popmail.py
@@ -0,0 +1,34 @@
+#!/usr/local/bin/python
+
+#e.g. 13-18
+
+import poplib, getpass, sys, mailconfig
+
+
+mailserver = mailconfig.popservername
+mailuser = mailconfig.popusername
+mailpasswd = getpass.getpass('Password for %s:' % mailserver)
+
+print('connecting...')
+server = poplib.POP3(mailserver)
+server.user(mailuser)
+server.pass_(mailpasswd)
+
+try:
+    print(server.getwelcome())
+    msgCount, msgBytes = server.stat()
+    print(msgCount, 'Mails; Total', msgBytes, 'bytes')
+    print(server.list())
+    print('-'*80)
+    input('[Press Enter]')
+
+    for i in range(msgCount):
+        hdr, message, octets = server.retr(i+1)
+        for line in message:
+            print(line.decode())
+        print('-'*80)
+        if i < msgCount - 1:
+            input('[Press Enter]')
+finally:
+    server.quit()
+print('Bye.')
