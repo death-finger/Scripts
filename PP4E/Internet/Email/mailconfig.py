493730e928c48882deba9727f3c5f912c3196631
Joshua Pu <death_finger@sina.com>
2016-04-11 17:04:44 +0800

    20160411 1705

--- PP4E/Internet/Email/mailconfig.py
+++ PP4E/Internet/Email/mailconfig.py
@@ -1,17 +1,17 @@
 # e.g. 13-17
 
 popservername = 'pop.sina.com'
-popusername = 'death_finger@sina.com'
+popusername = 'joshuapu@sina.com'
 
 smtpservername = 'smtp.sina.com'
 
-myaddress = 'death_finger@sina.com'
+myaddress = 'joshuapu@sina.com'
 mysignature = ('Best Regards,\nJoshua Pu')
 
-smtpuser = 'death_finger@sina.com'
-smtppasswdfile = 'Bagakira@881229'
+smtpuser = 'joshuapu@sina.com'
+smtppasswdfile = 'bagakira'
 
-poppasswdfile = 'Bagakira@881229'
+poppasswdfile = 'bagakira'
 
 sentmailfile = './sentmail.txt'
 savemailfile = './savemail.txt'
--- PP4E/test.py
+++ PP4E/test.py
@@ -1,26 +1,3 @@
-import imaplib, poplib, getpass
-
-
-# Init
-recv_server = input('[POP|IMAP]Address: ') or 'pop-mail.outlook.com'
-#send_server = input('[SMTP]Address: ')
-username = input('Username: ') or 'joshuapu@outlook.com'
-password = getpass.getpass('Password: ') or 'Password01!'
-
-if recv_server[:3] == 'pop':
-    conn = poplib.POP3_SSL(recv_server, 995)
-else:
-    conn = imaplib.IMAP4_SSL(recv_server, 993)
-
-conn.user(username)
-conn.pass_(password)
-
-# Recv mail
-try:
-    print(conn.getwelcome())
-    msgCount, msgBytes = conn.stat()
-    print('%s Mails\n%s Bytes in all' % (msgCount, msgBytes))
-    input('[Press Enter to continue]')
-    print(conn.list()[1].decode())
-finally:
-    conn.quit()
+消息 = '中文变量测试消息'
+打印 = print
+打印(消息)
