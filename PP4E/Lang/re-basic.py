86462cccda37292389b9413d571584197d5cf537
Joshua Pu <death_finger@sina.com>
2016-05-18 17:55:36 +0800

    update

--- PP4E/Lang/re-basic.py
+++ PP4E/Lang/re-basic.py
@@ -6,4 +6,8 @@ if matchobj:
     print(matchobj.start())
 
 pattobj = re.compile('A.*C.*')
-matchobj =
+matchobj = pattobj.search('xxABCDxx')
+if matchobj:
+    print(matchobj.start())
+
+print(re.search(' *A.C[DE][D-F][^G-ZE]G\t+ ?', '..ABCDEFG\t..').start())
